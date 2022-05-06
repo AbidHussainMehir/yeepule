d938700ef794cb75f0a2d1eb88e1d158e6bc6c97
abid <abid@abids-MacBook-Pro.local>
2022-05-06 22:23:53 +0500

    update public

--- src/utils/routes/publicRoutes.js
+++ src/utils/routes/publicRoutes.js
@@ -2,14 +2,15 @@ import React from "react";
 import { Redirect, Route } from "react-router-dom";
 function PublicRoute({ component: Component, ...restOfProps }) {
   const isAuthenticated = localStorage.getItem("isAuthenticated");
-  
+
   return (
     <Route
       {...restOfProps}
       render={(props) =>
-        isAuthenticated==='false' ?
-            <Component {...props} />
-          : <Redirect to="/dashboard" />
+        isAuthenticated === 'true' ?
+          <Redirect to="/dashboard" /> :
+          <Component {...props} />
+
       }
     />
   );
